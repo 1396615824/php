d70c2ab72c939dca51c0e77df184d6a63ede4ae9
1396615824 <40511240+1396615824@users.noreply.github.com>
2018-07-19 18:33:16 +0800

    添加文件
    a.php   a.php

---
+++ a.php
@@ -0,0 +1,11 @@
+<?php
+$mem=new memcache();
+// $m->connect('127.0.0.1',11212);
+$mem->addServer('127.0.0.1',11211);
+$mem->addServer('192.168.80.21',11211);
+$mem->addServer('192.168.80.64',11211);
+$mem->set('aa',147);
+$mem->set('bb',789);
+$mem->set('cc',258);
+$mem->set('dd',159);
+$mem->set('ee',369);
